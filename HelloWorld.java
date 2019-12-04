8df298d28318e4ed56970c0dfa544ffc8a0d93b6
shaiksha094 <shaiksha14346@gmail.com>
2019-12-04 08:17:53 +0530

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("Hello, Devops");
+    System.out.println("Hello, Banglore");
+  }
+}
